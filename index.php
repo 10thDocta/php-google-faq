e471ded17730e2ec0a3105d41e4ab12e111c08dd
Davide Preti <67058061+10thDocta@users.noreply.github.com>
2020-10-06 20:17:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -36,40 +36,47 @@ Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue info
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google Faq</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
 
-<header>
-    <div class="header_top">
-        <img src="img/logo.png" alt="">
-        <h4>Privacy e termini</h4>
-    </div>
+    <header>
+        <div class="header_top">
+            <img src="img/logo.png" alt="">
+            <h4>Privacy e termini</h4>
+        </div>
 
-    <div class="header_bottom">
-        <ul>
-            <li><a href="#">Introduzione </a></li>
-            <li><a href="#">Norme sulla privacy </a></li>
-            <li><a href="#">Termini di servizio </a></li>
-            <li><a href="#">Tecnologie </a></li>
-            <li><a href="#">Domande frequenti </a></li>
-        </ul>
-    </div>
-</header>
+        <div class="header_bottom">
+            <ul>
+                <li><a href="#">Introduzione </a></li>
+                <li><a href="#">Norme sulla privacy </a></li>
+                <li><a href="#">Termini di servizio </a></li>
+                <li><a href="#">Tecnologie </a></li>
+                <li><a class="active" href="#">Domande frequenti </a></li>
+            </ul>
+        </div>
+    </header>
 
 
-<main>
-    <div class="container">
-        <?php foreach ($questions as $q) { ?>
-        <h2> <?php echo $q["question"] ?></h2>
-        <p> <?php echo $q["answer"] ?></p>
-        <?php } ?>
-    </div>
-</main>
+    <main>
+        <div class="container">
+            <?php foreach ($questions as $q) { ?>
+            <section>
+                <h3> <?php echo $q["question"] ?></h3>
+                <p> <?php echo $q["answer"] ?></p>
+            </section>
+            <?php } ?>
+        </div>
+    </main>
+
+    <footer></footer>
 
 </body>
+
 </html>
